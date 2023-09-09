2ad398e0ba037262ae7ee54dd1413ff7561338da
r.trofimenko <r.trofimenko@net-vision.pro>
2023-09-09 14:32:50 +0300

    initial commit

---
+++ src/components/UI/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <h2>Главная страница</h2>
+    <n-divider />
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+    <n-card title="Card" >
+      Card Content
+    </n-card>
+    <n-input type="textarea" placeholder="Basic Textarea" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+.home-page {
+  .n-card {
+    border-radius: 1rem;
+  }
+}
+</style>
